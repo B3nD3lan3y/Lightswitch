24928725d861af2cb7c07bd326093e0080a947ea
Ben Delaney <bdelaney13@georgefox.edu>
2015-12-01 17:20:14 -0800

    Added some JavaScript. Drag start/stop (but not actual dragging) is implemented.

--- Lightswitch/www/scripts/index.js
+++ Lightswitch/www/scripts/index.js
@@ -1,4 +1,6 @@
-﻿// For an introduction to the Blank template, see the following documentation:
+﻿/// <reference path="../scripts/snap/snap.svg.js" />
+
+// For an introduction to the Blank template, see the following documentation:
 // http://go.microsoft.com/fwlink/?LinkID=397704
 // To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
@@ -22,5 +24,46 @@
     function onResume() {
         // TODO: This application has been reactivated. Restore application state here.
     };
+
+
+
+    const CONFIG = {
+        speed: 1.0,
+    };
+
+    var snap = Snap("svg");
+    var swatches = [snap.select("#swatch-1"), snap.select("#swatch-2")];
+    var theSwitch = snap.select("#switch");
+    var screenCircle = snap.select("#screen-circle");
+
+    theSwitch.drag(dragMove, dragStart, dragEnd);
+
+    function dragStart(x, y, event)
+    {
+        screenCircle.removeClass("off");
+        screenCircle.addClass("on");
+    }
+
+    function dragMove(dx, dy, x, y, event)
+    {
+
+    }
+
+    function dragEnd(event)
+    {
+        screenCircle.removeClass("on");
+        screenCircle.addClass("off");
+
+    }
+
+    function update()
+    {
+        var dx = theSwitch.transform().localMatrix.e;
+        var dy = theSwitch.transform().localMatrix.f;
+        for (swatch in swatches)
+        {
+            
+        }
+    }
 
 } )();
